22c26ca97512e8a8ac3ef272dddf4afe27e4de2b
CadmoNeto <cadmonneto@gmail.com>
2026-08-20 12:48:26 -0300

    feat: add core text chat endpoint

--- apps/core/src/ultron_core/api/chat.py
+++ apps/core/src/ultron_core/api/chat.py
@@ -4,10 +4,12 @@ from pydantic import BaseModel
 class ChatRequest(BaseModel):
     message: str
 
+class ChatResponse(BaseModel):
+    message: str
+
 chat_router = APIRouter()
 
-@chat_router.post("/chat")
+@chat_router.post("/chat", response_model=ChatResponse)
 async def chat(request: ChatRequest):
-    return {
-        "message": request.message
-    }
+    response = ChatResponse(message=request.message)
+    return response
